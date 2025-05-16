68c7bab8a7158cc9aa06750bb1e2980fb01d5027
s24808 <93827195+s24808@users.noreply.github.com>
2025-05-16 14:01:34 +0200

    Add files via upload

---
+++ Zadanie4/Zadania_kotlin/Main.kt
@@ -0,0 +1,18 @@
+package com.example.kotlinexercises
+
+class Main {
+fun main() {
+        zad1()
+        zad2()
+        zad3()
+        zad4()
+        zad5()
+        zad6()
+        zad7()
+        zad8()
+        zad9()
+        zad10()
+        zad11()
+        zad12()
+    }
+}
